33f4440e6aef02a681a20a4146dce36a344182c0
Dalton Okechukwu <chike911@yahoo.com>
2020-01-15 21:40:55 -0700

    refactored app.js for game login

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
 import "./App.css";
 import { Navigation } from "./components/Navigation/Navigation";
 import { Logo } from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
-// import { ImageLinkForm } from "./components/ImageLinkForm/ImageLinkForm";
-// import { FaceRecognition } from "./components/FaceRecognition/FaceRecognition";
 import SignIn from "./components/SignIn/SignIn";
 import Register from "./components/Register/Register";
 import Particles from "react-particles-js";
-import Clarifai from "clarifai";
 
-const app = new Clarifai.App({
-  apiKey: "8ee434780de44825a764987fa5900c9b"
-});
-
-const Fragment = React.Fragment;
 const particlesOptions = {
   Number: {
     value: 90,
@@ -43,12 +35,6 @@ class App extends Component {
       }
     };
   }
-  componentDidMount() {
-    this.setState({ imageUrl: "http://faces-unplugged.com/img/photo/008.jpg" });
-    fetch("http://localhost:3030/").then(response =>
-      response.json().then(data => console.log(data))
-    );
-  }
 
   addUser = data => {
     this.setState({
@@ -61,39 +47,10 @@ class App extends Component {
       }
     });
   };
-  calcFaceLocation = responseData => {
-    const clarifaiFace =
-      responseData.outputs[0].data.regions[0].region_info.bounding_box;
-    console.log(clarifaiFace);
-    const image = document.getElementById("inputImage");
-    const imageWidth = Number(image.width);
-    const imageHeight = Number(image.height);
-    console.log(imageWidth, imageHeight);
-    return {
-      leftCol: clarifaiFace.left_col * imageWidth,
-      topRow: clarifaiFace.top_row * imageHeight,
-      rightCol: imageWidth - clarifaiFace.right_col * imageWidth,
-      bottomRow: imageHeight - clarifaiFace.bottom_row * imageHeight
-    };
-  };
-
-  displayFaceBox = boxData => {
-    console.log(boxData);
-    this.setState({ faceBox: boxData });
-  };
 
   onInputChange = e => {
     console.log(e.target.value);
     this.setState({ userInput: e.target.value });
-  };
-
-  onButtonSubmit = e => {
-    console.log("detect button clicked");
-    this.setState({ imageUrl: this.state.userInput });
-    app.models
-      .predict(Clarifai.FACE_DETECT_MODEL, this.state.userInput)
-      .then(response => this.displayFaceBox(this.calcFaceLocation(response)))
-      .catch(error => console.log(error));
   };
 
   onRouteChange = route => {
@@ -121,14 +78,6 @@ class App extends Component {
                 name={this.state.user.name}
                 entries={this.state.user.entries}
               />
-              {/* <ImageLinkForm
-                onInputChange={this.onInputChange}
-                onButtonSubmit={this.onButtonSubmit}
-              />
-              <FaceRecognition
-                faceBox={this.state.faceBox}
-                imageUrl={this.state.imageUrl}
-              /> */}
             </div>
           ) : this.state.route === "signin" ? (
             <SignIn addUser={this.addUser} onRouteChange={this.onRouteChange} />
