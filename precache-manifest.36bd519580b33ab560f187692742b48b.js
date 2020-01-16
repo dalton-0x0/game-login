da2fa751d7f5545988b95089d64933ce649ff43f
Dalton Okechukwu <chike911@yahoo.com>
2020-01-15 22:49:53 -0700

    Updates

---
+++ precache-manifest.36bd519580b33ab560f187692742b48b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eefc736718067732cfd1717505ebae9f",
+    "url": "/game-login/index.html"
+  },
+  {
+    "revision": "538c80b35b1b45a4a164",
+    "url": "/game-login/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a4f22aaba88443886d39",
+    "url": "/game-login/static/css/main.411f7629.chunk.css"
+  },
+  {
+    "revision": "538c80b35b1b45a4a164",
+    "url": "/game-login/static/js/2.3c5be5d3.chunk.js"
+  },
+  {
+    "revision": "e92e356b467ce5268290e03394dbf217",
+    "url": "/game-login/static/js/2.3c5be5d3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a4f22aaba88443886d39",
+    "url": "/game-login/static/js/main.2f1f7af2.chunk.js"
+  },
+  {
+    "revision": "871a37b43a8bf74e150a",
+    "url": "/game-login/static/js/runtime-main.dffb2a9e.js"
+  }
+]);
